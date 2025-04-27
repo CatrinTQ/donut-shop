559e482cf2ed45e77c344040d808baeda4125152
Catrin Tornqvist <catrin.tq@gmail.com>
2025-04-27 12:23:22 +0200

    Add cart and start view

---
+++ src/components/DonutCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="card">
+    <img src="">
+    <h2>Donut name</h2>
+    <p>Pris: </p>
+    <p>Betyg:</p>
+    <div>
+        <button>-</button>
+        <p>0 st</p>
+        <button>+</button>
+    </div>
+</div>
+
+</template>
+
+<style scoped>
+.card {
+    border: solid black 1px;
+    border-radius: 10px;
+    max-width: 300px;
+}
+</style>
---
+++ src/views/StartView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue';
+import DonutCard from '@/components/DonutCard.vue';
+</script>
+
+<template>
+    <TheHeader />
+    <DonutCard />
+</template>
