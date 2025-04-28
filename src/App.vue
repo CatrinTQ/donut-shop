27076a6c391595ea0c031d017d0b0d1596e47e9a
Catrin Tornqvist <catrin.tq@gmail.com>
2025-04-28 12:48:53 +0200

    Small fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
 import StartView from './views/StartView.vue';
 
 </script>
 
 <template>
+  <TheHeader />
   <StartView />
+  <TheFooter />
 </template>
 
 <style scoped>
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheHeader from '@/components/TheHeader.vue';
 import DonutCard from '@/components/DonutCard.vue';
 </script>
 
 <template>
-    <TheHeader />
     <DonutCard />
 </template>
