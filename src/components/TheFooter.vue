67f05a94845543d243ad3aff8b50c0ba717ca318
Catrin Tornqvist <catrin.tq@gmail.com>
2025-04-28 12:48:06 +0200

    Add footer component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer>
+        <h2>Kawaii donut</h2>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+    background-color: lightblue;
+}
+
+</style>
