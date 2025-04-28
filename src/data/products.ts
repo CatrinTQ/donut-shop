28f1d16bcc82d91a65111a489949e596473cebb4
Catrin Tornqvist <catrin.tq@gmail.com>
2025-04-28 12:47:39 +0200

    Add data for products

---
+++ src/data/products.ts
@@ -0,0 +1,215 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    rating: number;
+    category: string;
+    amount: number;
+    img: {
+        url: string;
+        width: number;
+        height: number;
+        alt: string;
+    }
+}
+
+export const products: Product[] = [
+    {
+      id: 1,
+      name: 'Tiger',
+      price: 35,
+      rating: 4,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-1.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer en tiger'
+      },
+    },
+    {
+      id: 2,
+      name: 'Snölejon',
+      price: 30,
+      rating: 5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-2.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer ett snölejon'
+      },
+    },
+    {
+      id: 3,
+      name: 'Blå tiger',
+      price: 250,
+      rating: 2.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-3.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer een blå tigen'
+      },
+    },
+    {
+      id: 4,
+      name: 'Kanin',
+      price: 40,
+      rating: 1.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-4.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer en kanin'
+      },
+    },
+    {
+      id: 5,
+      name: 'Matcha',
+      price: 40,
+      rating: 3.5,
+      amount: 0,
+      category: 'special',
+      img: {
+        url: 'src/img/product-image-5.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med matchasmak'
+      },
+    },
+    {
+      id: 6,
+      name: 'Dino',
+      price: 40,
+      rating: 5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-6.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer en dinosaurie'
+      },
+    },
+    {
+      id: 7,
+      name: 'Persika',
+      price: 40,
+      rating: 4.5,
+      amount: 0,
+      category: 'special',
+      img: {
+        url: 'src/img/product-image-7.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med persikosmak'
+      },
+    },
+    {
+      id: 8,
+      name: 'Spöke',
+      price: 30,
+      rating: 3.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-8.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer ett spöke'
+      },
+    },
+    {
+      id: 9,
+      name: 'Gris',
+      price: 25,
+      rating: 3.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-9.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer en gris'
+      },
+    },
+    {
+      id: 10,
+      name: 'Pokemon',
+      price: 40,
+      rating: 4.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-10.png',
+        width: 120,
+        height: 120,
+        alt: 'pokémon inspirerad munk'
+      },
+    },
+    {
+      id: 11,
+      name: 'Nebulosa',
+      price: 40,
+      rating: 5,
+      amount: 0,
+      category: 'space',
+      img: {
+        url: 'src/img/product-image-11.png',
+        width: 120,
+        height: 120,
+        alt: 'donut looking lika a nebula'
+      },
+    },
+    {
+      id: 12,
+      name: 'Björn',
+      price: 35,
+      rating: 2.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: 'src/img/product-image-12.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer en björn'
+      },
+    },
+    {
+      id: 13,
+      name: 'Vintergatan',
+      price: 45,
+      rating: 4.5,
+      amount: 0,
+      category: 'space',
+      img: {
+        url: 'src/img/product-image-13.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer vintergatan'
+      },
+    },
+    {
+      id: 14,
+      name: 'Meteorregn',
+      price: 40,
+      rating: 3.5,
+      amount: 0,
+      category: 'space',
+      img: {
+        url: 'src/img/product-image-14.png',
+        width: 120,
+        height: 120,
+        alt: 'munk med frosting som föreställer ett meteorregn'
+      },
+    },
+  ];
+
+  export default products;
