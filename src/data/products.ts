41dfd4bfd29b4fe2e94685353cb7dcc35880973e
Catrin Tornqvist <catrin.tq@gmail.com>
2025-04-29 18:12:51 +0200

    update search path

--- src/data/products.ts
+++ src/data/products.ts
@@ -22,7 +22,7 @@ export const products: Product[] = [
       amount: 0,
       category: 'animal',
       img: {
-        url: '/donut-shop/img/product-image-1.png',
+        url: '/donut-shop/public/img/product-image-1.png',
         width: 120,
         height: 120,
         alt: 'munk med frosting som föreställer en tiger'
